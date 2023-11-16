eaa24c19e5bf56e45f963c0f643f7166dd6d334a
Arafath <arafathctk@gmail.com>
2023-11-16 15:16:59 +0530

    little errors resolved

--- A6.js
+++ A6.js
@@ -1,7 +1,6 @@
 function secondLargest(n) {
-    n.sort((a,b)=>(b-a))
-    return n[1]
+  n.sort((a, b) => b - a);
+  return n[1]; //do not work in an array which has duplicate elements
 }
 
-console.log(secondLargest([2, 2, 4, 6, 8, 9, 4]))
-
+console.log(secondLargest([2, 2, 4, 6, 8, 9, 4]));
---
+++ A7.js
@@ -0,0 +1,7 @@
+function duplicateRemove(array) {
+  console.log([...new Set(array)]);
+}
+
+let x = [2, 4, 3, 3, 5, 3, 7, 8, 2, 4, 9, 7, 6, 5, 4];
+duplicateRemove(x);
+console.log(x);
